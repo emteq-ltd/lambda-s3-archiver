1a1cf83a2179dae5daf56d8cb5dbfd4c8a268942
Dan Shirley <danshirley1@gmail.com>
2020-09-25 17:37:54 +0100

    Removed initial slash from 'path root' flag so you can supply none more cleanly.

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const s3 = new aws.S3();
  * @param {s3File} - The fully qualified S3 object key
  * @return {string} - The custom formatted file name
  */
- 
+
 /*
  * This nodejs module will read and archive files in AWS S3 bucket using stream, and store the archived file in S3 as well..
  * @param {sourceBucket} - the S3 bucket containing the files to archive
- * @param {sourcePath} - the S3 prefix/path containing the files to archive
+ * @param {sourcePath} - the S3 prefix/path containing the files to archive. Include a trailing slash (e.g. mydir/) - DS.
  * @param {sourceFiles} - (OPTIONAL) the list of filenames in the sourcePath to archive
  *                      - If not specified, all the files in sourcePath will be included in the archive
  * @param {outputFilename} - the filename of the archive file. Default to 'archive'.
@@ -33,7 +33,7 @@ const s3 = new aws.S3();
  */
 const archive = (
   sourceBucket,
-  sourcePath,
+  sourcePathPrefix,
   sourceFiles = [],
   outputFilename = 'archive',
   outputFormat = 'zip',
@@ -42,10 +42,11 @@ const archive = (
 ) => {
     return new Promise(async (resolve, reject) => {
         try {
+            const sourcePath = sourcePathPrefix || '';
             const format = (['zip', 'tar'].includes(outputFormat.toLowerCase()) ? outputFormat : 'zip');
             const streamArchiver = archiver(format);
 
-            const outputFilePath = `${sourcePath}/${outputFilename}.${format}`;
+            const outputFilePath = `${sourcePath}${outputFilename}.${format}`;
             const outputStream = new PassThrough();
 
             const params = {
@@ -70,21 +71,25 @@ const archive = (
             streamArchiver.pipe(outputStream);
 
             if (sourceFiles && sourceFiles.length > 0) {
-                sourceFiles = sourceFiles.map(file => `${sourcePath}/${file}`);
+                sourceFiles = sourceFiles.map(file => `${sourcePath}${file}`);
             } else {
-                var continuationToken = null
-                while (true) {
-                    // Include all files in the S3 sourcePath in the archive if sourceFiles is empty
-                    let s3Objects = await s3.listObjectsV2({ Bucket: sourceBucket, Prefix: sourcePath, ContinuationToken: continuationToken }).promise();
-                    continuationToken = s3Objects.NextContinuationToken;
-                    sourceFiles = sourceFiles.concat(s3Objects.Contents.map(content => { return content.Key; }).filter(k => k != `${sourcePath}/`));
-                    console.log(`Found ${sourceFiles.length} files in ${sourcePath}`);
-                    if (!s3Objects.IsTruncated) {
-                        break;
-                    }
-                }
+              var continuationToken = null
+              while (true) {
+                // Include all files in the S3 sourcePath in the archive if sourceFiles is empty
+                const s3ObjectsProps = { Bucket: sourceBucket, ContinuationToken: continuationToken };
 
-                console.log(`Found ${sourceFiles.length} total files in ${sourcePath}`);
+                if (sourcePath.length) s3ObjectsProps.Prefix = sourcePath;
+
+                let s3Objects = await s3.listObjectsV2(s3ObjectsProps).promise();
+                continuationToken = s3Objects.NextContinuationToken;
+                sourceFiles = sourceFiles.concat(s3Objects.Contents.map(content => { return content.Key; }).filter(k => k != `${sourcePath}`));
+                console.log(`Found ${sourceFiles.length} files in ${sourcePath}`);
+                if (!s3Objects.IsTruncated) {
+                    break;
+                }
+              }
+
+              console.log(`Found ${sourceFiles.length} total files in ${sourcePath}`);
             }
 
             console.log('Working with source files:', sourceFiles);
